2da2dd82755c77dd9161c3eb69f01a22764c7f3e
Diego Gonzalez <45858@psdschool.org>
2021-11-19 10:13:43 -0700

    User choice functioning

--- Rock Paper Scissors/ViewController.swift
+++ Rock Paper Scissors/ViewController.swift
@@ -7,13 +7,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController
+{
+    //Delcare Outlets and Variables
+    var images:[UIImage] = []
+    var userChoice = 1
+    
+    @IBOutlet weak var computerImage: UIImageView!
+    @IBOutlet weak var playerImage: UIImageView!
+    @IBOutlet var pictureCollection: [UIImageView]!
+    
+    
+    @IBOutlet weak var horizontalStackView: UIStackView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let rockImage = UIKit.UIImage(named:"rockPicture")
+        let paperImage = UIKit.UIImage(named:"paperPicture")
+        let scissorsImage = UIKit.UIImage(named:"scissorsPicture")
+        images = [rockImage, paperImage, scissorsImage] as! [UIKit.UIImage]
+        
+        
     }
-
+    let UIImage = "rockPicture"
+   //Need more!
+    @IBAction func printOk(_ sender: Any)
+    {
+        print("Ok")
+        let selectedPoint = (sender as! AnyObject) .location (in:horizontalStackView)
+       
+        for label in pictureCollection
+        {
+            if label.frame.contains(selectedPoint)
+            {
+                userChoice = label.tag
+                playerImage.image = images[userChoice]
+                
+                
+            }
+        }
+        
+    }
+    
 
 }
 
